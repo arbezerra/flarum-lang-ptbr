6b2ac788ec19945de657055d5c97421e01aeb24c
joao_cg <xjogos@gmail.com>
2019-04-23 17:30:40 -0300

    Traduzindo o moments js de forma sugerida na documentacao oficial do flarum

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,8 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
         $event->loadLanguagePackFrom(__DIR__);
+        if (file_exists($file = $localeDir.'/config.js')) {
+            $event->locales->addJsFile($locale, $file);
+        }
     });
 };
